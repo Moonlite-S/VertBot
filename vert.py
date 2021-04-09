631d809966c31d905d07fbe7c82ffdc8f7be3cf8
Moonlite-Irony <iphoneagents00@gmail.com>
2021-04-09 18:36:05 -0500

    added an env file.

--- vert.py
+++ vert.py
@@ -1,7 +1,9 @@
 import discord
 import os
+from discord.ext import commands
+from dotenv import load_dotenv
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '#')
 
 @client.event
 async def on_ready():
@@ -9,12 +11,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
 #Ignore any mesage that the bot sends
   if message.author == client.user:
     return
+
+#Channel Access Variables (Based on my personal server)
+  general_channel = client.get_channel(830170824115224577)
+
 
 #Simple response by saying hello
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
 
+
+#Loads up the env file to pass the token
+load_dotenv()
 client.run(os.getenv('DISCORD_TOKEN'))
